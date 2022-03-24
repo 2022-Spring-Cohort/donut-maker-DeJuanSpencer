b013717dea9a57112e487c9a4b2b3c81021b2cd5
DeJuanSpencer <dejuanspencer@gmail.com>
2022-03-23 21:45:20 -0400

    Iterations 2-5 finished

--- js/Donut.js
+++ js/Donut.js
@@ -2,31 +2,69 @@ class Donut {
   constructor(score) {
     this._score = 1000;
     this._level = 1;
+    this._numberOfClicks = 0;
     this._multiplierCount = 0;
     this._multiply = false;
     this._multiplierRate = 1 * Math.pow(1.2, this._multiplierCount);
     this._multiplierCost = 100 * Math.pow(1.1, this._multiplierCount);
+
+    this._clickIncrease = 1;
+    this._clickMultiplierCount = 0;
+    this._clickMultiply = false;
+    this._clickMultiplierRate = 1 * Math.pow(1.2, this._clickMultiplierCount);
+    this._clickMultiplierCost = 10 * Math.pow(1.1, this._clickMultiplierCount);
   }
 
   plusOne() {
-    this._score += 1;
+    this._clickIncrease = this.clickMultiplierRate;
+
+    console.log("Click: " + this._clickIncrease);
+    this._score += this._clickIncrease;
+    this._numberOfClicks += 1;
     return this._score.toFixed(1);
+  }
+
+  clickMultiplier() {
+    if (this._score >= 10) {
+      this._clickMultiply = true;
+    }
+
+    this._clickMultiplierCount += 1;
+    console.log("Score: " + this._score);
+    console.log("Clicker cost " + this._clickMultiplierCost);
+
+    this._score -= this._clickMultiplierCost;
+
+    this._clickMultiplierCost = this.updateClickMultiplierCost();
+    this._clickIncrease = this.clickMultiplierRate;
+    console.log("I have click multiplied");
   }
 
   multiplier() {
     if (this._score >= 100) {
       this._multiply = true;
     }
+    this._multiplierCount += 1;
     this._score -= this._multiplierCost;
     this._multiplierCost = this.updateMultiplierCost();
+
     console.log("I have multiplied");
   }
 
   updateMultiplierCost() {
-    this._multiplierCount += 1;
     this._multiplierCost = 100 * Math.pow(1.1, this._multiplierCount);
-
     return this._multiplierCost;
+  }
+
+  get clickMultiplierRate() {
+    this._clickMultiplierRate = 1 * Math.pow(1.2, this._clickMultiplierCount);
+    return this._clickMultiplierRate;
+  }
+
+  updateClickMultiplierCost() {
+    this._clickMultiplierCost = 10 * Math.pow(1.1, this._clickMultiplierCount);
+
+    return this._clickMultiplierCost;
   }
   multiplyPerSecond() {
     if (this._multiply) {
@@ -44,6 +82,18 @@ class Donut {
     } else {
       return (this._level = 3);
     }
+  }
+
+  get multiplierCount() {
+    return this._multiplierCount;
+  }
+
+  get clickMultiplierCount() {
+    return this._clickMultiplierCount;
+  }
+
+  get numberOfClicks() {
+    return this._numberOfClicks;
   }
 }
 export { Donut };
--- js/index.js
+++ js/index.js
@@ -4,12 +4,16 @@ const donut = new Donut();
 console.log(donut);
 
 const head = document.querySelector("head");
-const titleEl = document.createElement("title");
-titleEl.innerHTML = "This shows how many points you have";
-const headerEl = document.createElement("header");
-headerEl.innerHTML =
-  " <h1>Tribute to <a href= 'https://orteil.dashnet.org/cookieclicker/'> Cookie Clicker </a></h1>";
+
 const body = document.querySelector("body");
+const titleEl = document.createElement("title");
+
+titleEl.innerText = "This shows how many points you have";
+const headerEl = document.createElement("header");
+headerEl.classList.add("navigation");
+headerEl.innerHTML =
+  " <h1 onclick='CollapseExpandAllUtil.collapseExpandAll'>Tribute to <a href= 'https://orteil.dashnet.org/cookieclicker/' target = 'blank'> Cookie Clicker </a></h1>";
+
 const container = document.createElement("div");
 container.classList.add("container");
 const donutContainer = document.createElement("section");
@@ -18,6 +22,10 @@ const newDonutContainer = document.createElement("section");
 let nextMultiplier = document.createElement("div");
 nextMultiplier.classList.add("next-multiplier");
 let nextMultiplierCost = 100 + Math.pow(1.1, donut.multiplierCount);
+
+const clickMultiplier = document.createElement("button");
+clickMultiplier.innerText = "Bang For Your Click";
+clickMultiplier.classList.add("clickMultiplierButton");
 
 let nextDifference = nextMultiplierCost - donut.score;
 
@@ -43,6 +51,7 @@ donutContainer.appendChild(donutButton);
 showPoints.innerText = "Score: " + donut._score;
 
 autoClickers.appendChild(multiplier);
+autoClickers.appendChild(clickMultiplier);
 
 display.appendChild(autoClickers);
 display.appendChild(showPoints);
@@ -55,17 +64,70 @@ body.appendChild(container);
 body.appendChild(headerEl);
 head.appendChild(titleEl);
 
+const numberOfClicks = document.createElement("div");
+numberOfClicks.classList.add("clicksEl");
+numberOfClicks.innerHTML =
+  "This is how many clicks you have: " + donut.numberOfClicks;
+
+const numberOfAutoEl = document.createElement("div");
+numberOfAutoEl.classList.add("autoEl");
+numberOfAutoEl.innerHTML =
+  "This is how many autoclicks you have: " + donut.clickMultiplierCount;
+
+const numberOfMultiplierEl = document.createElement("div");
+numberOfMultiplierEl.classList.add("multiplierEl");
+numberOfMultiplierEl.innerHTML =
+  "This is how many multipliers you have: " + donut.multiplierCount;
+
+function updateStats() {
+  showPoints.innerText = "Score:" + donut._score;
+  numberOfClicks.innerHTML =
+    "This is how many clicks you have: " + donut.numberOfClicks;
+
+  numberOfAutoEl.innerHTML =
+    "This is how many autoclicks you have: " + donut.clickMultiplierCount;
+  numberOfMultiplierEl.innerHTML =
+    "This is how many multipliers you have: " + donut.multiplierCount;
+}
+
+const resetButton = document.createElement("button");
+resetButton.classList.add("resetButton");
+resetButton.innerText = "Reset";
+
+display.appendChild(resetButton);
+display.appendChild(numberOfClicks);
+
+display.appendChild(numberOfMultiplierEl);
+display.appendChild(numberOfAutoEl);
+
 donutButton.addEventListener("click", () => {
   let score = donut.plusOne();
   let soundEffect = new Audio("adf.wav");
-
-  showPoints.innerText = "Score: " + score;
+  showPoints.innerText = "Score:" + score;
 });
 
 multiplier.addEventListener("click", () => {
   if (donut._score >= donut._multiplierCost) {
     donut.multiplier();
+    let score = donut._score.toFixed(1);
+    showPoints.innerText = "Score:" + score;
+    "This is how many multipliers you have: " + donut.multiplierCount;
+    // console.log("Multiplier Count: " + donut._multiplierCount);
+    // console.log("Multiplier Rate: " + donut._multiplierRate);
+    // console.log("Multiplier Cost: " + donut._multiplierCost);
+    updateStats();
+  }
+});
+
+clickMultiplier.addEventListener("click", () => {
+  if (donut._score >= donut._clickMultiplierCost) {
+    donut.clickMultiplier();
+    let score = donut._score.toFixed(1);
+
     showPoints.innerText = "Score:" + donut._score;
+    numberOfAutoEl.innerHTML =
+      "This is how many autoclicks you have: " + donut.clickMultiplierCount;
+    updateStats();
     // console.log("Multiplier Count: " + donut._multiplierCount);
     // console.log("Multiplier Rate: " + donut._multiplierRate);
     // console.log("Multiplier Cost: " + donut._multiplierCost);
@@ -74,7 +136,7 @@ multiplier.addEventListener("click", () => {
 
 setInterval(() => {
   if (donut._multiply) {
-    donut.multiplyPerSecond();
+    donut.plusOne();
     let currentScore = donut._score;
     showPoints.innerText = "Score: " + currentScore.toFixed(1);
     let difference = Math.round(donut._multiplierCost - donut._score);
@@ -86,5 +148,15 @@ setInterval(() => {
     } else if (difference == 0) {
       nextMultiplier.innerText = "Click that multiply button!";
     }
+    updateStats();
   }
 }, 1000);
+
+resetButton.addEventListener("click", () => {
+  const reply = prompt(
+    "You are about to reset your progress. Are you sure? Type 'RESET' to reset your progress."
+  );
+  if (reply === "RESET") {
+    location.reload();
+  }
+});
